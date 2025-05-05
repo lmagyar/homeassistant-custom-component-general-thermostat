home-assistant/core

Fix generic_thermostat so it doesn't turn on when current temp is within target temp range (#138209)
* Don't turn on thermostat if temp is equal to target temp.

* Update strings to reflect logic change.

* Fix logic and add zero tolerance tests.

* Include tests for cool mode

* Removed unnecessary async_block_till_done calls

homeassistant/components/generic_thermostat/climate.py
@@ -539,10 +539,14 @@ class GenericThermostat(ClimateEntity, RestoreEntity):
                     return
 
             assert self._cur_temp is not None and self._target_temp is not None
-            too_cold = self._target_temp >= self._cur_temp + self._cold_tolerance
-            too_hot = self._cur_temp >= self._target_temp + self._hot_tolerance
+
+            min_temp = self._target_temp - self._cold_tolerance
+            max_temp = self._target_temp + self._hot_tolerance
+
             if self._is_device_active:
-                if (self.ac_mode and too_cold) or (not self.ac_mode and too_hot):
+                if (self.ac_mode and self._cur_temp <= min_temp) or (
+                    not self.ac_mode and self._cur_temp >= max_temp
+                ):
                     _LOGGER.debug("Turning off heater %s", self.heater_entity_id)
                     await self._async_heater_turn_off()
                 elif time is not None:
@@ -552,7 +556,9 @@ class GenericThermostat(ClimateEntity, RestoreEntity):
                         self.heater_entity_id,
                     )
                     await self._async_heater_turn_on()
-            elif (self.ac_mode and too_hot) or (not self.ac_mode and too_cold):
+            elif (self.ac_mode and self._cur_temp > max_temp) or (
+                not self.ac_mode and self._cur_temp < min_temp
+            ):
                 _LOGGER.debug("Turning on heater %s", self.heater_entity_id)
                 await self._async_heater_turn_on()
             elif time is not None:

lmagyar/homeassistant-custom-component-general-thermostat

custom_components/general_thermostat/climate.py
@@ -655,10 +655,14 @@ class GeneralThermostat(ClimateEntity, RestoreEntity, cached_properties=CACHED_P
                     return
 
             assert self._attr_current_temperature is not None and self._attr_target_temperature is not None
-            too_cold = self._attr_target_temperature >= self._attr_current_temperature + self._attr_cold_tolerance
-            too_hot = self._attr_current_temperature >= self._attr_target_temperature + self._attr_hot_tolerance
+
+            min_temp = self._attr_target_temperature - self._attr_cold_tolerance
+            max_temp = self._attr_target_temperature + self._attr_hot_tolerance
+
             if self._is_device_active:
-                if (self.ac_mode and too_cold) or (not self.ac_mode and too_hot):
+                if (self.ac_mode and self._attr_current_temperature <= min_temp) or (
+                    not self.ac_mode and self._attr_current_temperature >= max_temp
+                ):
                     _LOGGER.debug("Turning off heater %s", self.heater_entity_id)
                     await self._async_heater_turn_off()
                 elif time is not None:
@@ -669,7 +673,9 @@ class GeneralThermostat(ClimateEntity, RestoreEntity, cached_properties=CACHED_P
                     )
                     await self._async_heater_turn_on()
             else:
-                if (self.ac_mode and too_hot) or (not self.ac_mode and too_cold):
+                if (self.ac_mode and self._attr_current_temperature > max_temp) or (
+                    not self.ac_mode and self._attr_current_temperature < min_temp
+                ):
                     _LOGGER.debug("Turning on heater %s", self.heater_entity_id)
                     await self._async_heater_turn_on()
                 elif time is not None: